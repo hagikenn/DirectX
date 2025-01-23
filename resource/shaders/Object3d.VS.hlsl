3f7264b05fc5f97cec97eb353a169bfc163a7005
hagikenn <146914983+hagikenn@users.noreply.github.com>
2025-01-23 17:20:45 +0900

    k

--- resource/shaders/Object3d.VS.hlsl
+++ resource/shaders/Object3d.VS.hlsl
@@ -2,7 +2,31 @@
 
 struct TransformationMatrix
 {
-    float32_t4x4 WVP;
+    float32_t4x4 WVP;#include"Object3d.hlsli"
+
+    struct TransformationMatrix
+    {
+        float32_t4x4 WVP;
+        float32_t4x4 World;
+    };
+    ConstantBuffer<TransformationMatrix> gTranfsformationMatrix : register(b0);
+
+
+    struct VertexShaderInput
+    {
+        float32_t4 position : POSITION0;
+        float32_t2 texcoord : TEXCOORD0;
+        float32_t3 normal : NORMAL0;
+    };
+
+    VertexShaderOutput main(VertexShaderInput input)
+    {
+        VertexShaderOutput output;
+        output.position = mul(input.position, gTranfsformationMatrix.WVP);
+        output.texcoord = input.texcoord;
+        output.normal = normalize(mul(input.normal, (float32_t3x3) gTranfsformationMatrix.World));
+        return output;
+    }
     float32_t4x4 World;
 };
 ConstantBuffer<TransformationMatrix> gTranfsformationMatrix : register(b0);
